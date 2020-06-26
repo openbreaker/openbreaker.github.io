2f6e7c913ab295972880cc24b75ff047e3af8477
qingyiwebt <qingyiweb@gmail.com>
2020-06-26 11:15:40 +0900

    4

--- viewer.js
+++ viewer.js
@@ -4,7 +4,7 @@ $(function(){
             resolve(data)
         });
     }).then(function(hash_data){
-        for (let i = 0; i < 1048576; i++) {
+        for (let i = 0; i < 65536; i++) {
             let index = '' + i;
             let md5data = md5(index);
             if(md5data == hash_data){
